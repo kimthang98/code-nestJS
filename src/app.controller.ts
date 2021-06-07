33961787cf7dbbf69c46ac538de6f3c95c97bff2
thang98 <thang98@gmail.com>
2021-06-07 20:41:32 +0700

    code nestJS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,13 @@
-import { Controller, Get,Delete,Param} from '@nestjs/common';
+import { Controller, Get} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/home')
-  getHello(): string {
-    return this.appService.getHello()
-  }
-  
   @Get()
-  getHello1(): string {
-    return this.appService.getHello()
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    console.log(id)
-    return `<h1>${id}<h1>`
+  getHello(): string {
+    return this.appService.getHello(`<h1>thắng</h1>`);
   }
 }
 
